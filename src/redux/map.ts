865e62b041cb458fa524ce7d0ab35a32dd61e64b
Julius Rajala <juliusrajala@gmail.com>
2018-07-21 20:14:17 +0300

    Baseline for map-reducer.

--- src/redux/map.ts
+++ src/redux/map.ts
@@ -1,4 +1,10 @@
+import { IMapState, IRdxAction } from "src/redux/types";
 
-export default (state: IMap, action: IRdx) => {
-  const { type,  }
+export default (state: IMapState, action: IRdxAction) => {
+  const { type, payload } = action;
+  switch(type) {
+    case 'map.NEW_MARKER':
+    case 'map.PUSH_MARKER':
+    default: return state;
+  }
 }
